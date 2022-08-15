aa9397cb12fec636f5e268c4bfe7bf01e5d5f8da
Luis Gustavo Cardoso Zanetti Pereira <luisgustavoczp@gmail.com>
2022-08-15 18:14:36 +0000

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
 const port = process.env.PORT;
-const isSsl = process.env.SSL?process.env.SSL === "true" : false;
+const isSsl = process.env.SSL ? process.env.SSL == "true" : false;
 
 module.exports = {port, isSsl};
